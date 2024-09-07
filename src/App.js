f387eb353eae41c2eac55fbcb1e76eeb41479c31
Aarav Jain <137067931+Aarav-Jain123@users.noreply.github.com>
2024-09-07 22:18:10 +0530

    modified:   node_modules/.cache/.eslintcache 	new file:   node_modules/.cache/babel-loader/04023e275deb1b6796148030d6d5e6285acfa9d4e8a0e434d209712c5dc94acc.json 	new file:   node_modules/.cache/babel-loader/06b799b13c023db0e64fe02b94914824f439a3780ae8560656a6a1c307177c17.json 	new file:   node_modules/.cache/babel-loader/2678dfe70636f2fa85bb89c97a6be99b1b2db30ecfbc3c166e9a7a40c22b1fee.json 	new file:   node_modules/.cache/babel-loader/287ee7f394c2bfe875554f6d4f3c82c768fec6bbff3e9cac2b606b791063c80c.json 	new file:   node_modules/.cache/babel-loader/2bafc569c6868239840e89a2c79e83e2d00cf7e96b6564771f50009a05a7d8cd.json 	new file:   node_modules/.cache/babel-loader/302d17c5fa35eef3390e3da5114d7a340cda7752e98934a092eb831da1546173.json 	new file:   node_modules/.cache/babel-loader/74ff778322d9f711faf1f1da606a0204f30180d5984c1c3392931b00a730279b.json 	new file:   node_modules/.cache/babel-loader/90acec2fc6b2ef40223c2a4b5d356645af35e359cd6085c7b097f3399e0011e1.json 	new file:   node_modules/.cache/babel-loader/94fa4aec2e82dd3834c25658a4bd046e0897cbddf004922f025b53fa4639d2a7.json 	new file:   node_modules/.cache/babel-loader/95e06944506f5b3c749513126b13e077731a8a576f7dcd91337b5e9f7fb51527.json 	new file:   node_modules/.cache/babel-loader/96312bde5352d4e45838af92b79fe075556a309f8cb169e9b1adcddb4497857f.json 	new file:   node_modules/.cache/babel-loader/999efb280123afc943ef2e2d62df1dc45c69cffd7cd576c03a7d032954935978.json 	new file:   node_modules/.cache/babel-loader/a52dd9f98582166eed37a75561699cc92693cdc0b7ef8e4c1a8a70e58ef6bf61.json 	new file:   node_modules/.cache/babel-loader/a6adc34348dd6b69ed6e74997ade5bffd7d0ee306c72c628ced05cdbbac9be8e.json 	new file:   node_modules/.cache/babel-loader/ae4cdbdeae64b9ecefe056fe0039ae94b95d4baa83be126bf10c5f0816ca92f1.json 	new file:   node_modules/.cache/babel-loader/b685f3360a06c402b654aed9ffe66b6e10f51a6fffaf7940f77bce79ab345922.json 	new file:   node_modules/.cache/babel-loader/bdfe5557a799a8d961f67b1b87b8d4040216e6c9a4c1115a04a11eebae925bcb.json 	new file:   node_modules/.cache/babel-loader/c78e713ff5c8f6cf44ca7fce2fca418aded2950cbd0c844e97fa1e2d645cffbd.json 	new file:   node_modules/.cache/babel-loader/cc2e1ac64ca2cf333b954e5e6af114336fae03ff61f502d9380d1accf55f4e73.json 	new file:   node_modules/.cache/babel-loader/f4ba330b8849134ddfbb24b3b33aaa2091d06e4dea231400ad3dc3cc4258cecb.json 	modified:   node_modules/.cache/default-development/0.pack 	modified:   node_modules/.cache/default-development/1.pack 	new file:   node_modules/.cache/default-development/10.pack 	new file:   node_modules/.cache/default-development/11.pack 	new file:   node_modules/.cache/default-development/12.pack 	new file:   node_modules/.cache/default-development/13.pack 	modified:   node_modules/.cache/default-development/2.pack 	modified:   node_modules/.cache/default-development/3.pack 	modified:   node_modules/.cache/default-development/4.pack 	modified:   node_modules/.cache/default-development/5.pack 	modified:   node_modules/.cache/default-development/6.pack 	new file:   node_modules/.cache/default-development/7.pack 	new file:   node_modules/.cache/default-development/8.pack 	new file:   node_modules/.cache/default-development/9.pack 	modified:   node_modules/.cache/default-development/index.pack 	modified:   node_modules/.cache/default-development/index.pack.old 	modified:   src/App.js 	modified:   src/index.css 	new file:   src/script.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,30 @@ function HeaderComp(){
   )
 }
 
+function CardExpandedComp(){
+  return (
+    <section>
+      <div className='card-expanded'>
+        <div className='card-body'>
+        <h3 className='card-title'>Welcome user,</h3>
+        <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
 function CardComp(){
-  return
+  return 
 }
 
 function App() {
   return (
     <div className="App">
       <HeaderComp />
+      <CardExpandedComp />
     </div>
   );
 }
---
+++ src/script.js
@@ -0,0 +1,9 @@
+const url = 'http://worldtimeapi.org/api/timezone/America/New_York';
+
+async function getData(){
+    const res = await fetch(url);
+    const data = await res.json()
+    console.log(data);
+}
+
+getData();
