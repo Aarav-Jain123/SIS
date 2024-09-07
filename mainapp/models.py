dda1b5e5a478cc2fba974cca497c4498d7acd2c0
Aarav Jain <137067931+Aarav-Jain123@users.noreply.github.com>
2024-09-07 22:24:46 +0530

    modified:   mainapp/models.py 	modified:   node_modules/.cache/default-development/3.pack 	modified:   node_modules/.cache/default-development/4.pack 	modified:   node_modules/.cache/default-development/index.pack 	modified:   node_modules/.cache/default-development/index.pack.old

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,6 +6,15 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     country = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
+    location_data = models.TextField()
+    user_key = models.TextField()
+    
     
     def __str__(self):
         return self.user.email
+    
+class Soil(UserProfile):
+    soil_type = models.CharField(max_length=100)
+    soil_texture = models.CharField(max_length=100)
+    registerd_to = models.CharField(max_length=100)
+    
