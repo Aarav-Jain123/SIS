2f3d57bdc5139421e12473f604c436debad75649
Aarav Jain <137067931+Aarav-Jain123@users.noreply.github.com>
2024-09-09 19:38:31 +0530

    new file:   .venv/Lib/site-packages/dotenv/__init__.py 	new file:   .venv/Lib/site-packages/dotenv/__main__.py 	new file:   .venv/Lib/site-packages/dotenv/cli.py 	new file:   .venv/Lib/site-packages/dotenv/ipython.py 	new file:   .venv/Lib/site-packages/dotenv/main.py 	new file:   .venv/Lib/site-packages/dotenv/parser.py 	new file:   .venv/Lib/site-packages/dotenv/py.typed 	new file:   .venv/Lib/site-packages/dotenv/variables.py 	new file:   .venv/Lib/site-packages/dotenv/version.py 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/INSTALLER 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/LICENSE 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/METADATA 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/RECORD 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/REQUESTED 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/WHEEL 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/entry_points.txt 	new file:   .venv/Lib/site-packages/python_dotenv-1.0.1.dist-info/top_level.txt 	new file:   .venv/Scripts/dotenv.exe 	new file:   mainapp/weatherPuller.py

---
+++ mainapp/weatherPuller.py
@@ -0,0 +1,17 @@
+import requests
+from dotenv import load_dotenv
+import datetime
+import os
+# from .views import state, pin_code, city
+
+# def greet_user():
+load_dotenv()
+API_KEY = os.getenv('OPEN_WEATHER_MAP_API_KEY')
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = f'Faridabad, Haryana'
+request_url = f'{BASE_URL}?appid={API_KEY}&q={city}'
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    json_result = response.json()
