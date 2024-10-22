1396660473b6df5d1c460171225a062d0d2ab479
Aarav Jain <137067931+Aarav-Jain123@users.noreply.github.com>
2024-10-22 20:54:17 +0530

    modified:   node_modules/.cache/default-development/15.pack 	modified:   node_modules/.cache/default-development/17.pack 	modified:   node_modules/.cache/default-development/index.pack 	modified:   node_modules/.cache/default-development/index.pack.old 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function HeaderComp() {
       <nav className='navbar common-properties'>
         <ul>
           <li><a href='/'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" /></svg> <br /> <small>Home</small></a></li>
-          <li><a href='#'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M220-80q-58 0-99-41t-41-99q0-58 41-99t99-41q18 0 35 4.5t32 12.5l153-153v-110q-44-13-72-49.5T340-740q0-58 41-99t99-41q58 0 99 41t41 99q0 48-28 84.5T520-606v110l154 153q15-8 31.5-12.5T740-360q58 0 99 41t41 99q0 58-41 99t-99 41q-58 0-99-41t-41-99q0-18 4.5-35t12.5-32L480-424 343-287q8 15 12.5 32t4.5 35q0 58-41 99t-99 41Zm520-80q25 0 42.5-17.5T800-220q0-25-17.5-42.5T740-280q-25 0-42.5 17.5T680-220q0 25 17.5 42.5T740-160ZM480-680q25 0 42.5-17.5T540-740q0-25-17.5-42.5T480-800q-25 0-42.5 17.5T420-740q0 25 17.5 42.5T480-680ZM220-160q25 0 42.5-17.5T280-220q0-25-17.5-42.5T220-280q-25 0-42.5 17.5T160-220q0 25 17.5 42.5T220-160Z" /></svg> <br /> <small>Explain</small> </a></li>
-          <li><a href='#'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z" /></svg> <br /> <small>Contact</small></a></li>
+          {/* <li><a href='#'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M220-80q-58 0-99-41t-41-99q0-58 41-99t99-41q18 0 35 4.5t32 12.5l153-153v-110q-44-13-72-49.5T340-740q0-58 41-99t99-41q58 0 99 41t41 99q0 48-28 84.5T520-606v110l154 153q15-8 31.5-12.5T740-360q58 0 99 41t41 99q0 58-41 99t-99 41q-58 0-99-41t-41-99q0-18 4.5-35t12.5-32L480-424 343-287q8 15 12.5 32t4.5 35q0 58-41 99t-99 41Zm520-80q25 0 42.5-17.5T800-220q0-25-17.5-42.5T740-280q-25 0-42.5 17.5T680-220q0 25 17.5 42.5T740-160ZM480-680q25 0 42.5-17.5T540-740q0-25-17.5-42.5T480-800q-25 0-42.5 17.5T420-740q0 25 17.5 42.5T480-680ZM220-160q25 0 42.5-17.5T280-220q0-25-17.5-42.5T220-280q-25 0-42.5 17.5T160-220q0 25 17.5 42.5T220-160Z" /></svg> <br /> <small>Explain</small> </a></li>
+          <li><a href='#'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z" /></svg> <br /> <small>Contact</small></a></li> */}
         </ul>
       </nav>
     </header>
@@ -119,26 +119,26 @@ function IrrigationInfoComp() {
   );
 };
 
-function ControlIrrigationSystem() {
-  function postReq() {
-    const req = fetch(
-      'https://8c9dw08c-8000.inc1.devtunnels.ms/api/irr/',
-      {
-        headers: {
-          "Content-Type": 'application/json'
-        },
-        method: 'POST',
-      }
-    )
-  }
-  return (
-    <div className='card common-properties'>
-      <button className='btn btn-peace' id='irr-btn' onClick={postReq}>Irrigate</button>
-      <p>In order to create a balance in soil factors, sprinkler will irrigate for 20 mins. Once activated, it can't stop.</p>
-    </div>
-  )
+// function ControlIrrigationSystem() {
+//   function postReq() {
+//     const req = fetch(
+//       'https://8c9dw08c-8000.inc1.devtunnels.ms/api/irr/',
+//       {
+//         headers: {
+//           "Content-Type": 'application/json'
+//         },
+//         method: 'POST',
+//       }
+//     )
+//   }
+//   return (
+//     <div className='card common-properties'>
+//       <button className='btn btn-peace' id='irr-btn' onClick={postReq}>Irrigate</button>
+//       <p>In order to create a balance in soil factors, sprinkler will irrigate for 20 mins. Once activated, it can't stop.</p>
+//     </div>
+//   )
 
-}
+// }
 
 function FarmCareAIComp() {
   const [prompt, setPrompt] = useState({
@@ -249,9 +249,9 @@ function App() {
         <IrrigationInfoComp />
       </section>
 
-      <section>
+      {/* <section>
         <ControlIrrigationSystem />
-      </section>
+      </section> */}
 
       <section>
         <FarmCareAIComp />
